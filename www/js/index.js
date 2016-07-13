488ceaf2d340c10ede574421fdec2254e537c4a7
Tushar Vengurlekar <tushar.vengurlekar@bidchat.com>
2016-07-13 12:58:42 +0530

    Updated UI to Show Captured Image

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,10 @@
+var isPhotoSet = false;
+
+var pictureFromCamera = document.createElement("img");
+    pictureFromCamera.setAttribute("height", "150");
+    pictureFromCamera.setAttribute("width", "150");      
+    pictureFromCamera.setAttribute("id", "camera_image"); 
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -19,7 +26,8 @@ var app = {
  **/
 function buttonOneClicked() {
     
-    alert("One Clicked");
+    // alert("One Clicked");
+    getCameraPicture();
 }
 
 /**
@@ -27,7 +35,61 @@ function buttonOneClicked() {
  **/
 function buttonTwoClicked() {
     
-    alert("Two Clicked");
+    if(isPhotoSet == false) {
+        setPicture();
+    }
+    else {
+        clearPicture();
+    }
 }
+
+function setPicture() {
+    pictureFromCamera.setAttribute("src", "images/logo.png");
+    document.getElementById("camera_pic").appendChild(pictureFromCamera);
+    isPhotoSet = true;
+}
+
+function setPicture(src) {
+    pictureFromCamera.setAttribute("src", src);
+    document.getElementById("camera_pic").appendChild(pictureFromCamera);
+    isPhotoSet = true;
+}
+
+function clearPicture() {      
+    var imageNode = document.getElementById("camera_image")
+    imageNode.parentNode.removeChild(imageNode);
+    isPhotoSet = false;
+}
+
+/**
+ * Default Implemenetion of Capture Picture from Camera Gallery
+ */      
+function getCameraPicture() {
+    
+    var cameraSourceType = navigator.camera.PictureSourceType;
+    var cameraSourceTypeSaved = cameraSourceType.SAVEDPHOTOALBUM; // {PHOTOLIBRARY: 0, CAMERA: 1, SAVEDPHOTOALBUM: 2} = $1
+    var destinationType = navigator.camera.DestinationType; // {DATA_URL: 0, FILE_URI: 1, NATIVE_URI: 2}
+    
+    var options = {
+        quality: 50,
+        destinationType: destinationType.FILE_URI,
+        correctOrientation: true,
+        sourceType: cameraSourceTypeSaved
+    }
+       
+    var onSuccess = function(result) {
+        console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+        console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+        
+        setPicture(result);
+    }
+
+    var onError = function(msg) {
+        console.log("Sharing failed with message: " + msg);
+    }
+
+    navigator.camera.getPicture(onSuccess, onError, options);
+}
+
 
 app.initialize();
