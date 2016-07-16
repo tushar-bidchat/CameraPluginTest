34a435e445dc4c58b7657e01973fde57e9022729
Tushar Vengurlekar <tushar.vengurlekar@bidchat.com>
2016-07-16 23:03:37 +0530

    Platform Android Updated

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/bidchat-plugin-imagecrop/www/crop.js",
+        "id": "bidchat-plugin-imagecrop.CropPlugin",
+        "clobbers": [
+            "plugins.crop"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
         "id": "cordova-plugin-camera.Camera",
         "clobbers": [
@@ -27,22 +34,15 @@ module.exports = [
         "clobbers": [
             "CameraPopoverHandle"
         ]
-    },
-    {
-        "file": "plugins/bidchat-plugin-imagecrop/www/crop.js",
-        "id": "bidchat-plugin-imagecrop.CropPlugin",
-        "clobbers": [
-            "plugins.crop"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "bidchat-plugin-imagecrop": "1.0.0",
     "cordova-plugin-compat": "1.0.0",
     "cordova-plugin-camera": "2.2.1-dev",
-    "cordova-plugin-whitelist": "1.2.2",
-    "bidchat-plugin-imagecrop": "1.0.0"
+    "cordova-plugin-whitelist": "1.2.2"
 };
 // BOTTOM OF METADATA
 });
